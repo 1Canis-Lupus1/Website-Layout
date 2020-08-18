d7f1cae384017a1995f963b3599f29b5e95a5c60
Shubham <ciruslivingstone@gmail.com>
2020-08-18 20:32:16 +0530

    Bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Main from './components/Main';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>React App Created</h2>
+      <h2 className="badge badge-primary">React App Created</h2>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main=()=>{
+    return(
+        <React.Fragment>
+            This is my main componnet!!
+        </React.Fragment>
+    );
+}
+
+export default Main;
