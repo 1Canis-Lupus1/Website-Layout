6b99f845538022c5fb87ee22ee23aafb0dedbbac
Shubham <ciruslivingstone@gmail.com>
2020-08-19 11:36:22 +0530

    All images and background color added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,7 @@ const Main=()=>{
 
     return(
         <React.Fragment>
-            <div class="container">
+            <div className="container m-2" style={{paddingTop:"40px"}}>
             <div class="row">
 
                 {/* First Column */}
@@ -36,11 +36,11 @@ const Main=()=>{
                         </div>
                     </div>
                     <div className="row">
-                        <div className="jumbotron rounded col-4 m-3" style={{height:"15vh"}}>
-                            Image
+                        <div className="jumbotron rounded col-4 m-3" style={{height:"27vh",backgroundColor:"#E8E007"}}>
+                            <img src="https://previews.123rf.com/images/bessyana/bessyana1809/bessyana180900092/108192106-colourful-cartoon-6-points-golden-star-.jpg" height="30" width="30" className="float-left mr-2"/><strong>1800 </strong>points
                         </div>
-                        <div className="jumbotron rounded col-4 m-3" style={{height:"15vh"}}>
-                            Image
+                        <div className="jumbotron rounded col-5 m-3" style={{height:"27vh",backgroundColor:"#B0F7F7"}}>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTbT9CJBaI7a9xTzhWXQrw1ZtSts8F3-LGQ1A&usqp=CAU" height="30" width="30" className="float-left mr-2"/><strong>45.3% </strong>complete
                         </div>
                     </div>
                 </div>
@@ -115,19 +115,29 @@ const Main=()=>{
                             <button className="m-2 btn btn-warning btn-sm rounded">Find</button>
                         </div><br/><br/><br/>
                     <div className="jumbotron rounded" style={{backgroundImage:"url(https://images4.alphacoders.com/109/thumb-350-1096657.jpg)",height:"30vh",backgroundSize:"cover"}}>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS1DeyZNqRdLF9WiyJOo7YQW5HxbSp3F6tNQQ&usqp=CAU" height="30" width="30" className="float-left mr-2"/><strong><h6 className="badge badge-info" style={{color:"white"}}>Rwds Agency</h6></strong><span className="float-right badge badge-warning">62 mins</span><br/>
+                        <p className="rounded" style={{backgroundColor:"#F3358A",padding:"10px",color:"white"}}>Making a Beautiful Scene with New Presets, created by Senior Illustrator</p>
                     </div>
                     <div className="row">
                         <div className="jumbotron rounded col-5 m-2" style={{backgroundImage:"url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQq6RDOv9ZeB3_QLnYHnqkSLnvcqIXLSYmACg&usqp=CAU)",height:"35vh",backgroundSize:"cover"}}>
-                            <span style={{color:"#0A35ED"}}>More Image Here</span>
+                            <span style={{color:"white"}}>
+                                <img src="https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg" height="20" widht="20" className="float-left mr-2"/>
+                                <strong><h6>Uran Design</h6></strong><span className="badge badge-secondary">49 mins</span>
+                                <p className="rounded" style={{backgroundColor:"#E4D41F",padding:"10px",color:"black"}}><h6>Great Tutorials!!</h6></p>
+                            </span>
                         </div>
                         <div className="jumbotron rounded col-5 m-2" style={{backgroundImage:"url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_HUEM4-cZ-z0cloBHPfSqwuQ9vlzs6hFwkw&usqp=CAU)",height:"35vh",backgroundSize:"cover"}}>
-                            <span style={{color:"#ED0A55"}}>More Image Here</span>
+                            <span style={{color:"black"}}>
+                                <img src="https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg" height="20" widht="20" className="float-left mr-2"/>
+                                <strong><h6>Uran Design</h6></strong><span className="badge badge-secondary">53 mins</span>
+                                <p className="rounded" style={{backgroundColor:"#E4D41F",padding:"10px"}}><h6 style={{color:"black"}}>Great Themes!!</h6></p> 
+                            </span>
                         </div>
                     </div><br/>
                     <strong>Special Offers</strong><strong style={floating}>...</strong><br/><br/>
                     <div className="jumbotron rounded m-2" style={{backgroundImage:"url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDLnyI77vEn0x6cX_l2j-dA7SkPXlzv6iVLg&usqp=CAU)",height:"50vh",backgroundSize:"cover"}}>
                         <h4><strong>50 Days Of Premium!</strong></h4>
-                        <h6>Get it before 01.09.2020</h6>
+                        <h6 className="badge badge-danger">Get it before 01.09.2020</h6>
                         <br/><br/><br/>
                         <button className="btn btn-info">Learn More</button>
                     </div>
